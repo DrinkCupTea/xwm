5e6b8787af530dcfbd29a5ead1acd62d5f945d75
XieWeijie <xieweijie9812@126.com>
2024-09-19 02:19:40 +0800

    convert some macro to inline method, change format

--- include/dwm.h
+++ include/dwm.h
@@ -9,6 +9,7 @@
 #include <X11/Xutil.h>
 #include <X11/cursorfont.h>
 #include <X11/keysym.h>
+#include <math.h>
 #include <sys/types.h>
 #include <sys/wait.h>
 #include <unistd.h>
@@ -236,9 +237,12 @@ extern Window root, wmcheckwin;
 
 /* macros */
 #define BUTTONMASK (ButtonPressMask | ButtonReleaseMask)
-#define CLEANMASK(mask) (mask & ~(numlockmask | LockMask) & (ShiftMask | ControlMask | Mod1Mask | Mod2Mask | Mod3Mask | Mod4Mask | Mod5Mask))
-#define INTERSECT(x, y, w, h, m)                                                                                                                    \
-    (MAX(0, MIN((x) + (w), (m)->wx + (m)->ww) - MAX((x), (m)->wx)) * MAX(0, MIN((y) + (h), (m)->wy + (m)->wh) - MAX((y), (m)->wy)))
+#define CLEANMASK(mask)                                                                                               \
+    (mask & ~(numlockmask | LockMask) &                                                                               \
+     (ShiftMask | ControlMask | Mod1Mask | Mod2Mask | Mod3Mask | Mod4Mask | Mod5Mask))
+#define INTERSECT(x, y, w, h, m)                                                                                      \
+    (MAX(0, MIN((x) + (w), (m)->wx + (m)->ww) - MAX((x), (m)->wx)) *                                                  \
+     MAX(0, MIN((y) + (h), (m)->wy + (m)->wh) - MAX((y), (m)->wy)))
 #define ISVISIBLE(C) ((C->tags & C->mon->tagset[C->mon->seltags]))
 #define LENGTH(X) (sizeof X / sizeof X[0])
 #define MOUSEMASK (BUTTONMASK | PointerMotionMask)
--- include/util.h
+++ include/util.h
@@ -4,9 +4,17 @@
 
 #include <stddef.h>
 
-#define MAX(A, B) ((A) > (B) ? (A) : (B))
-#define MIN(A, B) ((A) < (B) ? (A) : (B))
-#define BETWEEN(X, A, B) ((A) <= (X) && (X) <= (B))
+inline constexpr auto MAX(auto a, auto b) {
+    return a > b ? a : b;
+}
+
+inline constexpr auto MIN(auto a, auto b) {
+    return a > b ? b : a;
+}
+
+inline constexpr auto BETWEEN(auto x, auto a, auto b) {
+    return (a <= x && x <= b);
+}
 
 void die(const char* fmt, ...);
 void* ecalloc(size_t nmemb, size_t size);
